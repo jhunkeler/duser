92f5117ad21883e031137fa913489bb0c84396df
Joseph Hunkeler <jhunk@stsci.edu>
2011-04-19 07:38:49 -0400

    Initial commit

---
+++ compat.c
@@ -0,0 +1,59 @@
+/**
+* duser - Manage MajorDomo lists
+* Copyright (C) 2011 Joseph Hunkeler <jhunkeler@gmail.com, jhunk@stsci.edu>
+*
+* This file is part of duser.
+*
+* duser is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* duser is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with duser. If not, see <http://www.gnu.org/licenses/>.
+**/
+
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include "compat.h"
+
+#ifndef HAVE_STRCHRNUL
+#undef strchrnul
+char *strchrnul(const char* s, int c)
+{
+    while(*s && *s != c)
+    {
+        s++;
+    }
+    return (char*)s;
+}
+#endif
+
+#ifndef HAVE_STRCASESTR
+#undef strcasestr
+char *strcasestr(const char *s, const char *find)
+{
+    char c, sc;
+    size_t len;
+
+    if ((c = *find++) != 0) {
+        c = tolower((unsigned char)c);
+        len = strlen(find);
+        do {
+            do {
+                if ((sc = *s++) == 0)
+                    return (NULL);
+            } while ((char)tolower((unsigned char)sc) != c);
+        } while (strncasecmp(s, find, len) != 0);
+        s--;
+    }
+    return ((char *)s);
+}
+#endif
+
